72db7ab61215cef57d18141a206a0cb20a78bb53
davis66 <davis.dsouza@adapty.com>
2019-07-08 12:24:17 +0530

    slider updated detailing remaining

---
+++ javascript/jquery.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.sliding_section_1').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:true,
+  });
+})
+// $('.slick-next').slick({
+//     content: '<i class="fa fa-arrow-right"></i>'
+    
+// })
+
+// $('.slick-prev').slick({
+//     content: 'next'    
+// })
